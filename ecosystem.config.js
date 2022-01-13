3e531b64c7678229143b1d929ddac639d03389b1
dhfhfk <dhfhfk1203@gmail.com>
2022-01-13 15:21:37 +0900

    PM2 config 교체

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: "Scheme",
+            script: "./index.js",
+            log_date_format: "YYYY-MM-DDTHH:mm:ss",
+        },
+    ],
+};
